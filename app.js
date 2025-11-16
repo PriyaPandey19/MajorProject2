5124298de7ae6482631618a8708a2fc6295b3948
PriyaPandey19 <pp479021@gmail.com>
2025-11-16 21:04:44 +0530

    changes

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
 app.use("/", noteRoutes);
 app.use("/pages", notePageRoutes);
-app.use(express.static("public"));
 
 
 app.get("/",(req,res) =>{
